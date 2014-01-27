6adc435aabbf28daffcafaf4086e4bf5ac288537
ritz-netbook <ritz@ritz-netbook>
2014-01-27 15:03:37 +0100

    more functionality in wavtools, synthesis, ADSR.

--- compress-flatten.py
+++ compress-flatten.py
@@ -3,15 +3,18 @@ from wavtools import *
 from scipy import ndimage
 
 # make pretty dark colourscheme for pylab
-rc('axes',facecolor='k', edgecolor='0.8', linewidth=1)
+rc('axes',facecolor='k', edgecolor='0.8', linewidth=1, labelcolor='#ccff00')
 rc('grid',color='0.8')
+rc('text',color='#ccff00')
 rc('figure', facecolor='k',edgecolor='k')
 rc('savefig', facecolor='k',edgecolor='0.8',extension='png') # does not have any effect?
 rc('xtick', color='#ccff00')
 rc('ytick', color='#ccff00')
 
-def wave_ax():
+def wave_ax(title='wave'):
+    '''init nice axis for plotting waveforms'''
     cla()
+    pyplot.title(title)
     xticks([])
     yticks([-1,0,1])
     grid(True)
@@ -19,45 +22,87 @@ def wave_ax():
     #tight_layout()
 
 
-wav = wavread('d:\\mix\\collected voice clips\\reverse-the-polarity-[cut].wav')
-L = len(wav)
 
-wav2 = wav ** 2
-wav2 /= wav2.max()
+def track_envelope(wav, smoothing_factor=400):
+    wav2 = wav ** 2
+    # wav2 /= wav2.max()
+    smoothing_factor = 500
+    env = empty_like(wav2)
+    ndimage.filters.maximum_filter1d(wav2, smoothing_factor, output=env)
+    env = sqrt(ndimage.filters.gaussian_filter1d(env, smoothing_factor / 2))
+    # env /= env.max()
+    return env
 
-smoothing_factor = 450
-env = sqrt(ndimage.filters.gaussian_filter1d(wav2, smoothing_factor))
-env /= env.max()
-
-def kneemax(x1,x2,p=8):
+def knee(x1,x2,p=8):
+    '''Knee function, approaches max/min for positive/negative p.'''
     return (x1**p + x2**p) ** (1./p)
     
-def comp_gain(x, limit, maxfun=partial(kneemax, p=8)):
-    return limit / maxfun(x, limit)
-
-x = arange(L)
-
-figure(2)
-cla();
-xx=frange(0,1,npts=256)
-limit = 0.3
-plot(xx, comp_gain(xx, limit, maxfun=maximum), c='.6', lw=3)
-knee8 = partial(kneemax, p=8)
-plot(xx, comp_gain(xx, limit, maxfun=knee8), c='#00ff00', lw=1.5)
-axis([0,1,0,1])
-xticks([0,limit,1])
-yticks([0,limit,1])
-grid(True)
+def sigmoid(x, treshold=0.25, p=36):
+    return 1 / (1 + exp((-x + treshold) * p))
     
-figure(1)
-wave_ax()
-a_env = fill_between(x, -env, env, facecolor='#002244', edgecolor='#003366', lw=1.5)
-a_wav = plot(x,wav,c='#00ccff',lw=0.5, alpha=0.4)
+def comp_gain(x, target=partial(knee, .4, p=-8)):
+    return target(x) / x
 
-gain = comp_gain(env, limit, maxfun=knee8)
+# read input sound
+import os
+#wav = wavread('d:\\mix\\collected voice clips\\reverse-the-polarity-[cut].wav', to_mono=True)
+
+#filename = 'd:\\mix\\collected voice clips\\DickBrain[Coupling02x10].wav'
+#filename = 'd:\\mix\\renoise samples\\beat-aknifeandafork.wav' 
+filename = 'd:\\mix\\collected voice clips\\reverse-the-polarity-[cut].wav'
+wav = wavread(filename, to_mono=True)
+L = len(wav)
+
+# get with the COMPRESSION
+
+limit = 0.25
+
+target_k = partial(knee, limit, p=-8)       # soft compression fn (with knee)
+#target_k = lambda x: knee(x**2, .35**2, p=-8) / (.35**2)
+target_m = partial(minimum, limit)          # hard compression fn
+
+env = track_envelope(wav)
+gain = comp_gain(env, target_k)
+
 cenv = env * gain
 cwav = wav * gain
-a_cenv = fill_between(x, -cenv, cenv, facecolor='#009966', edgecolor='#00bb77', lw=1.5)
-a_cwav = plot(x ,cwav, c='#ccff55', lw=0.7)
+menv = track_envelope(cwav)
 
-show()
+def comp_draw_garphs():
+    # DRAW GARPHS
+
+    figure(2)
+    xx=frange(0,1,npts=257)[1:]
+
+    subplot2grid((2,4),(0,0))
+    cla();
+    title('target')
+    plot(xx, target_m(xx), c='.5', lw=4, alpha=0.6)
+    plot(xx, target_k(xx), c='#ccff00', lw=1.5)
+    axis([0,1,0,1])
+    xticks([0,limit,1])
+    yticks([0,limit,1])
+    grid(True)
+
+    subplot2grid((2,4),(1,0))
+    cla();
+    title('gain')
+    plot(xx, comp_gain(xx, target_m), c='.5', lw=4,alpha=0.6)
+    plot(xx, comp_gain(xx, target_k), c='#ccff00', lw=1.5)
+    axis([0,1,0,1])
+    xticks([0,limit,1])
+    yticks([0,limit,1])
+    grid(True)
+        
+    subplot2grid((2,4),(0,1),colspan=3,rowspan=3)
+    wave_ax(os.path.basename(filename))
+
+    x = arange(L)
+    fill_between(x, -env, env, facecolor='#002244', edgecolor='#003366', lw=1.5)
+    plot(x, wav, c='#00ccff', lw=0.5, alpha=0.4)
+
+    fill_between(x, -menv, cenv, facecolor='#009966', edgecolor='#00bb77', lw=1.5)
+    plot(x ,cwav, c='#ccff55', lw=0.7)
+
+    tight_layout()
+    show()
--- wavtools.py
+++ wavtools.py
@@ -3,18 +3,21 @@ from scipy.io import wavfile
 import pygame
 
 tau = 2*pi
-SR = 44100.0
+sr = 44100.0
 
-print '''SR = %.0f Hz.''' % SR
+print '''sr = %.0f Hz.''' % sr
+
+def a2sound(ar, level=12000.0):
+    return pygame.sndarray.make_sound((level * ar).astype('int16'))
 
 def play(ar, level=0.25):
     '''Play the array as a wave through pygame, normalized to a level 0..1.'''
-    global SR
+    global sr
     pygame.mixer.quit()
     if ar.ndim == 1:
-        pygame.mixer.init(frequency=int(SR), size=-16, channels=1)
+        pygame.mixer.init(frequency=int(sr), size=-16, channels=1)
     elif ar.ndim == 2 and ar.shape[1] == 2:
-        pygame.mixer.init(frequency=int(SR), size=-16, channels=2)
+        pygame.mixer.init(frequency=int(sr), size=-16, channels=2)
     else:
         print "ERR: array must be 1D (mono) or 2D (stereo) with two colums."
         return
@@ -30,21 +33,103 @@ def wav_float_to_int16(ar, level=32760):
     '''Convert float array to int16 array, normalized to a level 0..32767.'''
     return normalize(ar, level=level).astype('int16')
 
-def wavread(infile):
-    '''Load a WAV, normalized to 1.0.'''
-    global SR
-    SR_, wav = wavfile.read(infile)
-    SR_ = float(SR)
-    if SR_ != SR:
-        SR = SR_
-        print '''SR = %.0f Hz.''' % SR    
+def wav_float_to_int32(ar, level=32760 * 32760):
+    '''Convert float array to int32 array, normalized to a level 0..32767.'''
+    return normalize(ar, level=level).astype('int32')
+    
+def wavread(infile, to_mono=False):
+    '''Load a WAV, normalized to 1.0, optionally converted to mono.'''    
+    global sr
+    
+    sr_, wav = wavfile.read(infile)
+
+    if to_mono and wav.ndim == 2:
+        wav = sum(wav,axis=1)
+
+    sr_ = float(sr)
+    if sr_ != sr:
+        sr = sr_
+        print '''sr = %.0f Hz.''' % sr    
+
     return normalize(wav, 1.0)
     
-def wavwrite(ar, outfile, level=32760):
+def wavwrite(ar, outfile, level=32760 * 32760):
     '''Write a WAV, normalized.'''
-    global SR
-    wavfile.write(outfile, SR, wav_float_to_int16(ar, level))
+    global sr
+    wavfile.write(outfile, sr, wav_float_to_int32(ar, level))
 
 def normalize(ar, level=1.0):
     return ar * (float(level) / abs(ar).max())
     
+
+def sinewave(f, phi, L):
+    '''Returns L samples of a sinewave of frequency f (Hz) and phase phi.'''
+    res = arange(phi, phi + L)
+    res *= (f * tau / sr)
+    res = sin(res)
+    return res
+
+def beep(freq_phase_amp, L):
+    '''Additive synthesis of sinewaves.
+
+    freq_phase_amp -- a numpy array with a row for each sinewave, and
+    frequency, phase and amplitude in each column.
+    L -- length in samples.
+    '''
+    res = zeros(L)
+    ii = arange(L)
+    tmp = empty(L)
+    for f, p, a in freq_phase_amp:
+        multiply(ii, f * tau / sr, tmp)
+        add(tmp, p, tmp)        
+        sin(tmp, tmp)
+        multiply(tmp, a, tmp)
+        add(res, tmp, res)
+
+    return res
+    
+def gen_basic_waveforms():
+    f0 = 49.0; w0 = round(sr/f0); f0 = sr / w0
+    path = 'd:\\mix\\audio samples instruments\\basic-waveforms\\'
+    name = 'square_rnd_phase-%03dh-G2-(i).wav'
+    clf()
+    for ii,N_harmonics in enumerate([8,16,32,64]):
+        H = 1.0 + 2 * arange(N_harmonics)
+        waev = beep(c_[f0 * H, random(N_harmonics) * tau, 1 / H], 8 * w0)
+        subplot(3,4,ii+1);plot(waev)
+        wavwrite(waev, path + (name % N_harmonics))
+        
+def saw_bass(n, L=4400, ADSR=(100,500,.7,100), N_harmonics=32):
+    H = 1.0 + arange(N_harmonics)
+    f0 = 440 * 2**((n - 69) / 12.0)
+    saw = beep(c_[f0 * H, H*0, 1 / H], L)
+    A,D,S,R = ADSR
+    saw *= interp(arange(L),[0,A,A+D,L-R,L-1],[0,1,S,S,0])
+    return saw
+
+def play_melody(notes, samples):
+    ch = pygame.mixer.find_channel(True)
+    EV = pygame.USEREVENT + 1
+    ch.set_endevent(EV)
+    for n in notes:
+        while ch.get_busy() and pygame.event.wait() != EV:
+            pass
+        ch.queue(samples[n])
+
+def saw_pad(NN, L=80000, fullness=9, sep=0, ADSR=(3000, 10000, .6, 15000)):
+    I = arange(L, dtype=float)
+    SAW = empty(L, dtype=float)
+    r = zeros(L + sep * (len(NN)-1))
+    A,D,S,R = ADSR
+    ENV = interp(arange(L),[0,A,A+D,L-R,L-1],[0,1,S,S,0])
+    for i in range(fullness):
+        for ni,n in enumerate(NN):
+            SAW = I * (freq(n + rand() * .04 - .02) / sr)
+            SAW += rand()
+            SAW %= 1.0
+            SAW -= 0.5
+            SAW *= ENV
+            r[ni * sep:ni * sep + L] += SAW
+    return r
+
+
